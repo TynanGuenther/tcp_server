d7ddcec44769b8f82e997cbb8c5bdba43bbaa0e3
TynanGuenther <tgunther701@gmail.com>
2026-02-04 20:47:16 -0600

    adding select() to multiple clients

--- server.cpp
+++ server.cpp
@@ -1,16 +1,21 @@
+#include <cstring>
 #include <iostream>
 #include <sys/types.h>
 #include <unistd.h>
 #include <arpa/inet.h>
 #include <sys/socket.h>
 #include <sys/select.h>
+#include <csignal>
 
-
-
+volatile sig_atomic_t keep_running = 1;
+void handle_sigint(int) {
+    keep_running = 0;
+}
 
 int main() {
     const int PORT_NUM = 8080;
-    bool running = true;
+
+    signal(SIGINT, handle_sigint);
 
     int server_fd = socket(AF_INET, SOCK_STREAM, 0);
     if (server_fd < 0) {
@@ -44,61 +49,63 @@ int main() {
     FD_SET(server_fd, &master_set);
     int max_fd = server_fd;
 
-    while(running){
+    while(keep_running){
 	read_set = master_set;
 
-	select(max_fd + 1, &read_set, nullptr, nullptr, nullptr);
+	int ready = select(max_fd + 1, &read_set, nullptr, nullptr, nullptr);
+	if (ready < 0) {
+	    if (errno == EINTR)
+		continue;
+	    perror("select");
+	    break;
+	}
 
 	for (int fd = 0; fd <= max_fd; fd++) {
-	    if (FD_ISSET(fd, &read_set)) {
-		if (fd == server_fd) {
-		    //new connection
+	    if (!FD_ISSET(fd, &read_set)) {
+		continue;
+	    }
+	    if (fd == server_fd) {
+		sockaddr_in client_addr{};
+		socklen_t addr_len = sizeof(client_addr);
+		
+		int client_fd = accept(server_fd, (sockaddr*)&client_addr, &addr_len);
+		if (client_fd < 0) {
+		    perror("accept");
+		    continue;
+		}
+		FD_SET(client_fd, &master_set);
+		if (client_fd > max_fd)
+		    max_fd = client_fd;
+		std::cout << "New Client Connected!\n";
+	    } else {
+		char recieve[1024];
+		ssize_t bytes = recv(fd, recieve, sizeof(recieve) - 1, 0);
+
+		if (bytes <= 0){
+		    if (bytes <0)
+			perror("recv");
+
+		    close(fd);
+		    FD_CLR(fd, &master_set);
+		    std::cout << "Client disconnected\n";
 		} else {
-		    //existing client sent data
+		    recieve[bytes] = '\0';
+		    std::cout << "Recieved: " << recieve << std::endl;
+
+		    const char reply[] = "Message recieved\n";
+		    send(fd, reply, strlen(reply), 0);
 		}
 	    }
 	}
-	//TODO: IMPLEMENT NEW CONNECTION AND EXISTING
-
-	sockaddr_in client_addr{};
-	socklen_t addr_len = sizeof(client_addr);
-	int client_fd = accept(server_fd, (sockaddr*)&client_addr, &addr_len);
-	if (client_fd < 0) {
-	    perror("accept");
-	    continue;
-	}
-
-	char client_addr_str[INET_ADDRSTRLEN];
-	if (!inet_ntop(AF_INET, &client_addr.sin_addr, client_addr_str, sizeof(client_addr_str))) {
-	    perror("inet_ntop");
-	    close(client_fd);
-	    continue;
-	}
-
-	int client_port_num = ntohs(client_addr.sin_port);
-
-	char client_mess[1024];
-	ssize_t bytes_rec = recv(client_fd, client_mess, sizeof(client_mess)-1, 0);
-	if (bytes_rec <= 0) {
-	    if (bytes_rec < 0){
-		perror("recv");
-	    }
-	    close(client_fd);
-	    continue;
-	}
-
-	client_mess[bytes_rec] = '\0';
-	std::cout << "Recieved From: " << client_addr_str << ":" << client_port_num << " -> " << client_mess << std::endl;
-
-	char response[1024];
-	int response_len = snprintf(response, sizeof(response), "You said: %s", client_mess);
-
-	if (send(client_fd, &response, response_len, 0) < 0) {
-	    perror("send");
-	}
-
-	close(client_fd);
     }
+    std::cout << "\nShutting Down Server...\n";
+
+    for (int fd = 0; fd <= max_fd; fd++) {
+	if (FD_ISSET(fd, &master_set)){
+	    close(fd);
+	}
+    }
+
     close(server_fd);
 
     return 0;
